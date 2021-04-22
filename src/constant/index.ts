7f1e1b25b5aae90a95da01e4b6dbc225f6707153
Mateusz <mateusz.splawski96@gmail.com>
2021-04-22 22:02:17 +0200

    feat(constant): Add actionBar constants

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,26 +1,31 @@
 import { Add } from "views/Add/Add";
 import { Home } from "views/Home/Home";
 
-export const routes = [
-  {
+export const routes = {
+  home: {
     name: "Home",
     path: "/",
     routerPath: "/",
     component: Home,
     exact: true,
   },
-  {
+  add: {
     name: "Add character",
     path: "/add",
     routerPath: "/add",
     component: Add,
     exact: false,
   },
-  {
+  edit: {
     name: "Edit character",
     path: "/edit",
     routerPath: "/edit/:id",
     component: Add,
     exact: false,
   },
-];
+};
+
+export const actionBar = {
+  textField: { label: "Search character", name: "searchbar" },
+  button: "Add new character",
+};
